1ea2db4a8728e44de9c3545d0a9df1cece17ee37
Yen-Teh Liu <Tommy@Liuyen-tehdeMacBook-Pro.local>
2016-04-08 14:34:20 -0600

    codeigniter framework

---
+++ application/controllers/Teacher.php
@@ -0,0 +1,15 @@
+<?php
+
+class Teacher extends User {
+
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+  function viewcourse(){
+
+  }
+
+}
+?>
---
+++ application/models/dataload.php
@@ -0,0 +1,25 @@
+<?php
+Class Dataload extends CI_Model
+{
+	function login_db($username, $password)
+	{
+		$this -> db -> select('id, username, password');
+		$this -> db -> from('users');
+		$this -> db -> where('username = ' . "'" . $username . "'"); 
+		$this -> db -> where('password = ' . "'" . MD5($password) . "'"); 
+		$this -> db -> limit(1);
+
+		$query = $this -> db -> get();
+
+		if($query -> num_rows() == 1)
+		{
+			return $query->result();
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+}
+?>
